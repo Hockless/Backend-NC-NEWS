d40b9eebb20735552306caba3945e8ea7fc1fc89
Hockless <jameshockless97@gmail.com>
2022-01-18 16:06:19 +0000

    Written several .get endpoints along with the tests, model and controller

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,11 +1,12 @@
 const db = require('../db/connection.js');
 const testData = require('../db/data/test-data/index.js');
-const { seed } = require('../db/seeds/seed.js');
+const seed = require('../db/seeds/seed.js');
 const request = require('supertest');
+const app = require('../app');
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
-describe('get Topics', () => {
+describe('GET/ API', () => {
 	test('responds with message all ok', () => {
 		return request(app)
 			.get('/api')
@@ -16,34 +17,85 @@ describe('get Topics', () => {
 	});
 });
 
-describe('', () => {
-	test('', () => {});
+describe('GET TOPICS', () => {
+	test('Responds with a list of topics', () => {
+		return request(app)
+			.get('/api/topics')
+			.expect(200)
+			.then((res) => {
+				expect(res.body.result).toHaveLength(3);
+				res.body.result.forEach((topic) => {
+					expect(topic).toEqual(
+						expect.objectContaining({
+							description: expect.any(String),
+							slug: expect.any(String),
+						})
+					);
+				});
+			});
+	});
 });
 
-describe('', () => {
-	test('', () => {});
+describe('GET ARTICLE ID', () => {
+	test('Responds with a list of article IDs', () => {
+		return request(app)
+			.get('/api/article/:article_id')
+			.expect(200)
+			.then((res) => {
+				expect(res.body.result).toHaveLength(12);
+				res.body.result.forEach((id) => {
+					expect(id).toEqual(
+						expect.objectContaining({
+							article_id: expect.any(Number),
+						})
+					);
+				});
+			});
+	});
 });
 
-describe('', () => {
-	test('', () => {});
+describe('GET ARTICLES', () => {
+	test('Responds with a list of articles', () => {
+		return request(app)
+			.get('/api/articles')
+			.expect(200)
+			.then((res) => {
+				expect(res.body.result).toHaveLength(12);
+				res.body.result.forEach((articles) => {
+					expect(articles).toEqual(
+						expect.objectContaining({
+							article_id: expect.any(Number),
+							author: expect.any(String),
+							body: expect.any(String),
+							created_at: expect.any(String),
+							title: expect.any(String),
+							topic: expect.any(String),
+						})
+					);
+				});
+			});
+	});
 });
+// describe('', () => {
+// 	test('', () => {});
+// });
 
-describe('', () => {
-	test('', () => {});
-});
+// describe('', () => {
+// 	test('', () => {});
+// });
 
-describe('', () => {
-	test('', () => {});
-});
+// describe('', () => {
+// 	test('', () => {});
+// });
 
-describe('', () => {
-	test('', () => {});
-});
+// describe('', () => {
+// 	test('', () => {});
+// });
 
-describe('', () => {
-	test('', () => {});
-});
+// describe('', () => {
+// 	test('', () => {});
+// });
 
-describe('', () => {
-	test('', () => {});
-});
+// describe('', () => {
+// 	test('', () => {});
+// });
